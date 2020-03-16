301827704e39a010a9975364d60137fc2310f070
darrengainza <52829389+darrengainza@users.noreply.github.com>
2020-03-16 15:01:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -25,3 +25,62 @@ const navSlide = () => {
 }
 
 navSlide();
+
+
+document.getElementById('button-report').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
+
+document.getElementById('button-report-1').addEventListener("click", function() {
+	document.querySelector('.bg-modal-1').style.display = "flex";
+});
+
+document.querySelector('.close-1').addEventListener("click", function() {
+	document.querySelector('.bg-modal-1').style.display = "none";
+});
+
+document.getElementById('button-report-2').addEventListener("click", function() {
+	document.querySelector('.bg-modal-2').style.display = "flex";
+});
+
+document.querySelector('.close-2').addEventListener("click", function() {
+	document.querySelector('.bg-modal-2').style.display = "none";
+});
+
+document.getElementById('button-report-3').addEventListener("click", function() {
+	document.querySelector('.bg-modal-3').style.display = "flex";
+});
+
+document.querySelector('.close-3').addEventListener("click", function() {
+	document.querySelector('.bg-modal-3').style.display = "none";
+});
+
+document.getElementById('button-report-4').addEventListener("click", function() {
+	document.querySelector('.bg-modal-4').style.display = "flex";
+});
+
+document.querySelector('.close-4').addEventListener("click", function() {
+	document.querySelector('.bg-modal-4').style.display = "none";
+});
+
+document.getElementById('button-report-5').addEventListener("click", function() {
+	document.querySelector('.bg-modal-5').style.display = "flex";
+});
+
+document.querySelector('.close-5').addEventListener("click", function() {
+	document.querySelector('.bg-modal-5').style.display = "none";
+});
+
+document.getElementById('button-report-6').addEventListener("click", function() {
+	document.querySelector('.bg-modal-6').style.display = "flex";
+});
+
+document.querySelector('.close-6').addEventListener("click", function() {
+	document.querySelector('.bg-modal-6').style.display = "none";
+});
+
+
